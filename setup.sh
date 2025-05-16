78428c274ea74201020e98ff34f9e019caae3c82
Oliver Lee <108825799+oliverlee000@users.noreply.github.com>
2025-05-16 00:01:40 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -9,5 +9,5 @@ pip install requests==2.25.1
 pip install importlib-metadata==3.7.0
 pip install filelock==3.0.12
 pip install sklearn==0.0
-pip install tokenizers==0.15
+pip install tokenizers==0.21.0
 pip install explainaboard_client==0.0.7
