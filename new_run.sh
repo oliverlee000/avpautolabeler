4ad6db4bf1039457624f35eaeb53661c87098e69
oliverlee000 <oliver.lee@stanford.edu>
2025-05-16 11:49:38 -0700

    newurn

---
+++ new_run.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/usr/bin/python3 -m pip install --user pandas tokenizers==0.11.1 scikit-learn torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html transformers==4.29.2 safetensors==0.3.0
+/usr/bin/python3 main.py --train data/train.csv --dev data/dev.csv --use_gpu
