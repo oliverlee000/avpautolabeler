3c1a45996c297f01cae46851b4e294a39a9f21b2
oliverlee000 <oliver.lee@stanford.edu>
2025-05-16 11:36:48 -0700

    merger

--- run_code_with_env.sh
+++ run_code_with_env.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 env -i PATH=/usr/bin HOME=$HOME \
-    /usr/bin/python3 -m pip install --user pandas tokenizers==0.20.0 scikit-learn torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
+    /usr/bin/python3 -m pip install --user pandas tokenizers==0.20.0 scikit-learn torch==1.10.0+cu111 torchvision==0.10.0+cu111 torchaudio==0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
     /usr/bin/python3 main.py --train data/train.csv --dev data/dev.csv --use_gpu
